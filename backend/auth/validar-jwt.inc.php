660503ade11a9fae5bc941465aad88cdfc9a6851
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2022-12-08 17:45:55 -0300

    change: auth

--- backend/auth/validar-jwt.inc.php
+++ backend/auth/validar-jwt.inc.php
@@ -7,26 +7,26 @@ $token = @getallheaders()['authorization'];
 
 $responseBody = '';
 
-if($token) {
-    $decoded = JwtUtil::decode($token, JWT_SECRET_KEY);
-        
-} else {
-    http_response_code(401); // Não autorizado
+if(!isset($token)) {
+    http_response_code(401);
     $responseBody = '{ "message": "Sem token" }';
+} else {
+    try {
+        $decodedToken = JwtUtil::decode($token, JWT_SECRET_KEY);
+    } catch (Exception $e) {
+        // Muda o código de resposta HTTP para 'bad request'
+        http_response_code(401);
+        $responseBody = '{ "message": "Ocorreu um erro ao tentar executar esta ação. Erro: Código: ' .  $e -> getCode() . '. Mensagem: ' . $e -> getMessage() . '" }';
+    }
 }
 
-print_r($decoded);
-
-// Se existir $responseBody == não está autorizado
-if($responseBody) { // Mostrar a resposta para o cliente
-    $responseBody = '{ "message": "Token inválido" }';
-
-    // Defique que o conteúdo da resposta será um JSON (application/JSON)
+if ($responseBody) {
+    // Define que o conteúdo da resposta será um JSON (application/JSON)
     header('Content-Type: application/json');
-    
+
     // Exibe a resposta
     print_r($responseBody);
+
     exit; // Encerra o script
 }
-
 ?>
