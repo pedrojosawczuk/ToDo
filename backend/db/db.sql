f7cbb20fdfcd042d6177195de626a1f7f28170d2
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2022-12-09 22:27:33 -0300

    change: db attribute status to bool

--- backend/db/db.sql
+++ backend/db/db.sql
@@ -18,7 +18,7 @@ CREATE TABLE tb_todo (
     title VARCHAR(20) NOT NULL,
     description VARCHAR(255),
     deadline DATETIME,
-    status VARCHAR(1) NOT NULL default 'A',
+    status BOOL NOT NULL default TRUE,
     created_at DATETIME  DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     fk_user BIGINT NOT NULL,
@@ -38,18 +38,18 @@ INSERT INTO tb_user (name, email,  password)
     VALUES ('Juca da Silva', 'juca3@gmail.com', 1234);
 
 /* Add 3 ToDos tb_todo user 1 */
-INSERT INTO tb_todo (title, description, deadline, fk_user)
-    VALUES ('Teste', NULL, '2022-12-07 14:34:05', 'A', 1);
+INSERT INTO tb_todo (title, description, deadline, status, fk_user)
+    VALUES ('Teste', NULL, '2022-12-07 14:34:05', FALSE, 1);
 
 INSERT INTO tb_todo (title, description, deadline, status, fk_user)
-    VALUES ('Teste2', NULL, '2022-12-08 14:34:05', 'D', 1);
+    VALUES ('Teste2', NULL, '2022-12-08 14:34:05', TRUE, 1);
 
-INSERT INTO tb_todo (title, description, deadline, fk_user)
-    VALUES ('Teste3', NULL, '2022-12-09 14:34:05', 'A', 1);
+INSERT INTO tb_todo (title, description, deadline, status, fk_user)
+    VALUES ('Teste3', NULL, '2022-12-09 14:34:05', FALSE, 1);
 
 /* Add 2 ToDos tb_todo user 2 */
-INSERT INTO tb_todo (title, description, deadline, fk_user)
-    VALUES ('Teste', NULL, '2022-12-07 14:34:05', 'A', 2);
+INSERT INTO tb_todo (title, description, deadline, status, fk_user)
+    VALUES ('Teste', NULL, '2022-12-07 14:34:05', FALSE, 2);
 
-INSERT INTO tb_todo (title, description, deadline, fk_user)
-    VALUES ('Teste2', NULL, '2022-12-08 14:34:05', 'A', 2);
+INSERT INTO tb_todo (title, description, deadline, status, fk_user)
+    VALUES ('Teste2', NULL, '2022-12-08 14:34:05', FALSE, 2);
